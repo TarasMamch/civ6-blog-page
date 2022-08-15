c610eaec4285ff5109da11c49d909fb062edc854
Taras Mamchits <tmamchitspc@gmail.com>
2022-08-15 04:46:43 -0700

    added most of the front end code

---
+++ public/js/characterDetails.js
@@ -0,0 +1,124 @@
+const charName = document.getElementById('char-name')
+const charIcon = document.getElementById('char-icon')
+const civName = document.getElementById('civ-name')
+const civName2 = document.getElementById('civ-name2')
+const civIcon = document.getElementById('civ-icon')
+const civIcon2 = document.getElementById('civ-icon2')
+const charBonusName = document.getElementById('char-bonus-name')
+const charBonus = document.getElementById('char-bonus')
+const charBio = document.getElementById('char-bio')
+const civAbilityName = document.getElementById('civ-ability-name')
+const civAbility = document.getElementById('civ-ability')
+const civAbilityName2 = document.getElementById('civ-ability-name2')
+const civAbility2 = document.getElementById('civ-ability2')
+const unitIcon = document.getElementById('unit-icon')
+const unitName = document.getElementById('unit-name')
+const unitIcon2 = document.getElementById('unit-icon2')
+const unitName2 = document.getElementById('unit-name2')
+const unitAbility = document.getElementById('unit-ability')
+const unitDesc = document.getElementById('unit-description')
+const unitAbility2 = document.getElementById('unit-ability2')
+const unitDesc2 = document.getElementById('unit-description2')
+const buldingIcon = document.getElementById('building-icon')
+const buildingName = document.getElementById('building-name')
+const buildingAblility = document.getElementById('building-ability')
+const buildingDesc = document.getElementById('building-description')
+const buldingIcon2 = document.getElementById('building-icon2')
+const buildingName2 = document.getElementById('building-name2')
+const buildingAblility2 = document.getElementById('building-ability2')
+const buildingDesc2 = document.getElementById('building-description2')
+
+async function setPage() {
+    let data = await JSON.parse(localStorage.getItem('leader'))
+    let data2 = null
+    if (data == null) {
+        data = await JSON.parse(localStorage.getItem('secondLeader'))
+    } if (data == null) {
+        data = await JSON.parse(localStorage.getItem('special'))
+    }
+    let KeyName = window.localStorage.key(data)
+    console.log(data, data2)
+
+    if (KeyName == 'leader') {
+        charIcon.setAttribute('src', data.leaders[0].icon)
+        charName.innerHTML = data.leaders[0].name
+        civIcon.setAttribute('src', data.icon)
+        civName.innerHTML = data.name
+        charBonusName.innerHTML = `Leader Bonus - ${data.leaders[0].leader_bonus_name}`
+        charBonus.innerHTML = data.leaders[0].leader_bonus
+
+        charBio.innerHTML = data.leaders[0].bio
+        civAbilityName.innerHTML = data.ability_name
+        civAbility.innerHTML = data.ability
+        unitIcon.setAttribute('src', data.unit.icon)
+        unitName.innerHTML = data.unit.name
+        unitAbility.innerHTML = data.unit.ability
+        unitDesc.innerHTML = data.unit.description
+        buldingIcon.setAttribute('src', data.building.icon)
+        buildingName.innerHTML = data.building.name
+        buildingAblility.innerHTML = data.building.ability
+        buildingDesc.innerHTML = data.building.description
+        window.localStorage.clear()
+    } else if (KeyName == 'secondLeader') {
+        charIcon.setAttribute('src', data.leaders[1].icon)
+        charName.innerHTML = data.leaders[1].name
+        civIcon.setAttribute('src', data.icon)
+        civName.innerHTML = data.name
+        charBonusName.innerHTML = `Leader Bonus - ${data.leaders[1].leader_bonus_name}`
+        charBonus.innerHTML = data.leaders[1].leader_bonus
+
+        charBio.innerHTML = data.leaders[1].bio
+        civAbilityName.innerHTML = data.ability_name
+        civAbility.innerHTML = data.ability
+        unitIcon.setAttribute('src', data.unit.icon)
+        unitName.innerHTML = data.unit.name
+        unitAbility.innerHTML = data.unit.ability
+        unitDesc.innerHTML = data.unit.description
+        buldingIcon.setAttribute('src', data.building.icon)
+        buildingName.innerHTML = data.building.name
+        buildingAblility.innerHTML = data.building.ability
+        buildingDesc.innerHTML = data.building.description
+        window.localStorage.clear()
+    } else {
+        charIcon.setAttribute('src', data.leaders[0].icon)
+        charName.innerHTML = data.leaders[0].name
+        civIcon.setAttribute('src', data.icon)
+        civName.innerHTML = data.name
+        civIcon2.setAttribute('src', data.icon)
+        civName2.innerHTML = data.name
+        charBonusName.innerHTML = `Leader Bonus - ${data.leaders[0].leader_bonus_name}`
+        charBonus.innerHTML = data.leaders[0].leader_bonus
+
+        charBio.innerHTML = data.leaders[0].bio
+        civAbilityName.innerHTML = data.ability_name
+        civAbility.innerHTML = data.ability
+        unitIcon.setAttribute('src', data.unit.icon)
+        unitName.innerHTML = data.unit.name
+        unitAbility.innerHTML = data.unit.ability
+        unitDesc.innerHTML = data.unit.description
+        buldingIcon.setAttribute('src', data.building.icon)
+        buildingName.innerHTML = data.building.name
+        buildingAblility.innerHTML = data.building.ability
+        buildingDesc.innerHTML = data.building.description
+
+        data = await JSON.parse(localStorage.getItem('special2'))
+        civIcon2.setAttribute('src', data.icon)
+        civName2.innerHTML = data.name
+        civAbilityName2.innerHTML = data.ability_name
+        civAbility2.innerHTML = data.ability
+        unitIcon2.setAttribute('src', data.unit.icon)
+        unitName2.innerHTML = data.unit.name
+        unitAbility2.innerHTML = data.unit.ability
+        unitDesc2.innerHTML = data.unit.description
+        buldingIcon2.setAttribute('src', data.building.icon)
+        buildingName2.innerHTML = data.building.name
+        buildingAblility2.innerHTML = data.building.ability
+        buildingDesc2.innerHTML = data.building.description
+
+        window.localStorage.clear()
+    }
+}
+
+
+
+setPage()
---
+++ public/js/main.js
@@ -0,0 +1,128 @@
+const charCards = document.getElementById('character-display')
+
+async function fetchApiData() {
+    const response = await fetch("http://localhost:3003/api/leaders")
+    const leaders = await response.json()
+
+    createCards(leaders)
+}
+
+function createCards(leaders) {
+    for (let i = 0; i < leaders.length; i++) {
+        const card = document.createElement('div')
+        const charIcon = document.createElement('img')
+        const charName = document.createElement('span')
+        const civIcon = document.createElement('img')
+        const secondCivIcon = document.createElement('img')
+
+        if (leaders[i].civs.length === 1) {
+            charIcon.setAttribute('src', leaders[i].icon)
+            charName.innerHTML = leaders[i].name
+            civIcon.setAttribute('src', leaders[i].civs[0].icon)
+
+            card.appendChild(charIcon)
+            card.appendChild(charName)
+            card.appendChild(civIcon)
+            charCards.appendChild(card)
+            card.setAttribute('class', 'character-cards')
+        } else {
+            charIcon.setAttribute('src', leaders[i].icon)
+            charName.innerHTML = leaders[i].name
+            civIcon.setAttribute('src', leaders[i].civs[0].icon)
+            secondCivIcon.setAttribute('src', leaders[i].civs[1].icon)
+
+            card.appendChild(charIcon)
+            card.appendChild(charName)
+            card.appendChild(civIcon)
+            card.appendChild(secondCivIcon)
+            charCards.appendChild(card)
+            card.setAttribute('class', 'character-cards')
+        }
+    }
+}
+
+async function setCharacterPage(name) {
+    const response = await fetch("http://localhost:3003/api/civs")
+    const civs = await response.json()
+    const response2 = await fetch("http://localhost:3003/api/leaders")
+    const leaders = await response2.json()
+    console.log(civs)
+
+    if (name == 'Teddy Roosevelt (Rough Rider)') {
+        const a = []
+        const data = JSON.stringify(civs[0]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Qin Shi Huang') {
+        const a = []
+        const data = JSON.stringify(civs[8]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Victoria') {
+        const a = []
+        const data = JSON.stringify(civs[12]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Catherine de Medici (Magnificence)') {
+        const a = []
+        const data = JSON.stringify(civs[14]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Pericles') {
+        const a = []
+        const data = JSON.stringify(civs[19]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Gandhi') {
+        const a = []
+        const data = JSON.stringify(civs[22]);
+        a.push(data)
+        localStorage.setItem('secondLeader', a)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Eleanor of Aquitaine') {
+        const a = []
+        const b = []
+        const data = JSON.stringify(civs[12]);
+        const data2 = JSON.stringify(civs[14]);
+        a.push(data)
+        b.push(data2)
+        localStorage.setItem('special', a)
+        localStorage.setItem('special2', b)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else if (name == 'Kublai Khan') {
+        const a = []
+        const b = []
+        const data = JSON.stringify(civs[8]);
+        const data2 = JSON.stringify(civs[33]);
+        a.push(data)
+        b.push(data2)
+        localStorage.setItem('special', a)
+        localStorage.setItem('special2', b)
+        window.location.href = 'http://localhost:3003/character-page'
+    } else {
+        for (let i = 0; i < civs.length; i++) {
+            if (civs[i].leaders[0].name == name) {
+                const a = []
+                const data = JSON.stringify(civs[i]);
+                a.push(data)
+                localStorage.setItem('leader', a)
+                window.location.href = 'http://localhost:3003/character-page'
+            }
+        }
+    }
+
+}
+
+charCards.addEventListener("click", (e) => {
+    let name = e.target.children[1].outerText;
+    if (typeof name == 'string') {
+        setCharacterPage(name)
+    }
+});
+
+fetchApiData()
